ea114272002ddf75667bca1a204ffe6ccdf4ce47
KaneMJ <matthewjkane@live.com>
2018-08-04 07:49:11 -0700

    starting to work on the header

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div> This is a test, this is only a test of the emergency broadcasting system</div >
+        )
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-const title = 'New Text';
-
-ReactDOM.render(
-    <div>{title}</div>,
-    document.getElementById('App')
-);
+ReactDOM.render(<App />, document.getElementById('App'));
 
 module.hot.accept();
